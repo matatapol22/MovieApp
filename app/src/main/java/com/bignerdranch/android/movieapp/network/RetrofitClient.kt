010d8d21dae82c398b7ae5fb93fe50b2e7bf45fe
matatapol22 <matatapol22@gmail.com>
2025-03-04 21:52:20 +0700

    Создание файла RetrofitClient.kt

---
+++ app/src/main/java/com/bignerdranch/android/movieapp/network/RetrofitClient.kt
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.movieapp.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("http://www.omdbapi.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val apiService: ApiService = retrofit.create(ApiService::class.java)
+}

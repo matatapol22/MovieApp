5cb1a9fc543f41fc3cca47e2fae5ae49f8589eae
matatapol22 <matatapol22@gmail.com>
2025-03-04 21:27:09 +0700

    Настройки в build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.kapt)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -64,10 +63,12 @@ dependencies {
 
     implementation("androidx.room:room-runtime:2.6.1")
 
-    kapt("androidx.room:room-compiler:2.6.1")
-    
+    kapt("androidx.room:room-compiler:2.6.0")
+
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("com.google.android.material:material:1.11.0")
+
+}
